8a2ced4394fb6816dd69bce277e56997c91cdada
sandeepbandi924 <bandisandeep1374@gmail.com>
2023-11-04 18:52:41 +0530

    Logging and exception

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup,find_packages
+from typing import List
+
+HYPHEN_E_DOT = '-e .'
+
+def get_requirements(file_path:str) -> List[str]:
+   requirements = []
+   with open(file_path) as file_obj :
+      requirements = file_obj.readlines()
+      requirements = [req.replace('\n','') for req in requirements]
+
+      if HYPHEN_E_DOT in requirements:
+         requirements=requirements.remove(HYPHEN_E_DOT)
+
+   return requirements
+
+setup(
+   name='Census Income Prediction',
+   version='0.0.1',
+   author='Sandeep Bandi',
+   install_requires = get_requirements('requirements.txt'),
+   packages=find_packages()
+)
